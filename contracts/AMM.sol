175b38174861a1783fc740efe77777f4c93b9673
Tim Illguth <103483394+pttransamdriver@users.noreply.github.com>
2025-06-16 16:17:14 -0500

    Update AMM.sol as a finished product with re-entrance protection

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -16,20 +16,43 @@ contract AutomatedMarketMaker {
 
     // Accounting state variables. 
     uint256 public totalSharesCirculating; // This variable represents the total amount of shares that are cirulating or have been issued in the L-Pool. 
-    mapping(address => uint256) public userLiquidityShares; // This mapping represents the amount of shares that each user has in the L-Pool.
+    mapping(address => uint256) public userLiquidityShares; // This mapping represents the amount of shares that each user has in the L-Pool.    
     uint256 constant PRECISION = 10**18; // "PRECISION" is a constant variable and is therefor in all caps in keeping with the solidity style guide. The value is 10 to the 18 power (10**18, different from 10^18 which means something else in solidity).
+    bool locked;
+    modifier nonReentrant() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;        
+    }
     
 
     // Events
-    event swap(
-        address indexed user,  
-        address tokenSwapped,  
-        uint256 amountSwapped, 
-        address tokenReceived, 
-        uint256 amountReceived,      
-        uint256 newFirstTokenReserve, 
-        uint256 newSecondTokenReserve, 
-        uint256 timestamp 
+    event Swap(
+        address indexed user,
+        address tokenSwapped,
+        uint256 amountSwapped,
+        address tokenReceived,
+        uint256 amountReceived,
+        uint256 newFirstTokenReserve,
+        uint256 newSecondTokenReserve,
+        uint256 timestamp
+    );
+
+    event AddLiquidity(
+        address indexed provider,
+        uint256 firstTokenAmount,
+        uint256 secondTokenAmount,
+        uint256 liquiditySharestoMint,
+        uint256 timestamp
+    );
+
+    event RemoveLiquidity(
+        address indexed provider,
+        uint256 sharesRedeemed,
+        uint256 firstTokenAmount,
+        uint256 secondTokenAmount,
+        uint256 timestamp
     );
 
     /*
@@ -49,7 +72,9 @@ contract AutomatedMarketMaker {
         secondToken = _secondToken; // "secondToken" is the state variable and "_secondToken" is the parameter used for the internal contract use.
     }
 
-    function addLiquidity(uint256 _firstTokenAmount, uint256 _secondTokenAmount) external {
+    function addLiquidity(uint256 _firstTokenAmount, uint256 _secondTokenAmount) external
+        nonReentrant()
+    {
         // Externally called by the users or other contracts like metamask to add liquidity to the L-Pool. It interacts with the Token.sol contracts to transfer the tokens from the user to the L-Pool.
         require(
             firstToken.transferFrom(msg.sender, address(this), _firstTokenAmount), // Calls the builtin "transferFrom" function in the Token.sol contract and requires tokens to transfer from the user "msg.sender" to the L-Pool "address(this)" for the amount of "_firstTokenAmount".
@@ -84,45 +109,177 @@ contract AutomatedMarketMaker {
         constantProductK = firstTokenReserve * secondTokenReserve;
 
         // Update liquidity accounting
-        totalSharesCirculating += liquiditySharestoMint; // The total amount of shares that are cirulating plus the amount of shares that will be minted for the users deposit. Updating the State Variable. 
+        totalSharesCirculating += liquiditySharestoMint; // The total amount of shares that are cirulating plus the amount of shares that will be minted for the users deposit. Updating the State Variable.
         userLiquidityShares[msg.sender] += liquiditySharestoMint; // The total amount of shares that the user has in the L-Pool plus the amount of shares that will be minted for the users deposit. Updating the user's share balance.
 
+        // Emit AddLiquidity event
+        emit AddLiquidity(
+            msg.sender,
+            _firstTokenAmount,
+            _secondTokenAmount,
+            liquiditySharestoMint,
+            block.timestamp
+        );
         
     }
 
-    function calculateSecondTokenDeposit(uint256 _secondTokenAmount) public view 
-    returns (uint256 requiredFirstTokenAmount)
+    function calculateSecondTokenDeposit(uint256 _secondTokenAmount) public view
+        returns (uint256 requiredFirstTokenAmount)
     {
+        require(_secondTokenAmount > 0, "Second token amount must be greater than 0");
+        require(secondTokenReserve > 0, "Pool not initialized");
         requiredFirstTokenAmount = (firstTokenReserve * _secondTokenAmount) / secondTokenReserve; // Calculates the amount of firstToken that is required to be deposited to maintain the current pool ratio.
     }
 
-    function calculateFirstTokenDeposit(uint256 _firstTokenAmount) public view 
-    returns (uint256 requiredSecondTokenAmount)
+    function calculateFirstTokenDeposit(uint256 _firstTokenAmount) public view
+        returns (uint256 requiredSecondTokenAmount)
     {
+        require(_firstTokenAmount > 0, "First token amount must be greater than 0");
+        require(firstTokenReserve > 0, "Pool not initialized");
         requiredSecondTokenAmount = (secondTokenReserve * _firstTokenAmount) / firstTokenReserve; // Calculates the amount of secondToken that is required to be deposited to maintain the current pool ratio.
     }
 
-    function calculateTokenSwap(uint256 _firstTokenAmount) public view // View Function to see how many tokens you would receive if you swapped a certain amount of firstToken.
-    returns (uint256 secondTokenOut) 
+    function calculateFirstTokenSwap(uint256 _firstTokenAmount) public view // View Function to see how many tokens you would receive if you swapped a certain amount of firstToken.
+        returns (uint256 secondTokenOut)
     {
-        uint256 firstTokenAfterSwap = firstTokenReserve + _firstTokenAmount; // Calculates the amount of firstToken in the pool after the swap.
+        require(_firstTokenAmount > 0, "Swap amount must be greater than 0"); // Requires that the amount of firstToken that is being swapped is greater than 0.
+        require(firstTokenReserve > 0 && secondTokenReserve > 0, "Invalid pool reserves"); // Requires that the amount of firstToken and secondToken in the pool is greater than 0.
+        require(constantProductK > 0, "Pool not properly initialized");
+        uint256 amountInWithFee = (_firstTokenAmount * 997) / 1000; // 0.3% fee
+        uint256 firstTokenAfterSwap = firstTokenReserve + amountInWithFee; // Calculates the amount of firstToken in the pool after the swap.
+
+        require(firstTokenAfterSwap > 0, "Invalid swap amount: zero reserves after swap");
         uint256 secondTokenAfterSwap = constantProductK / firstTokenAfterSwap; // Calculates the amount of secondToken in the pool after the swap.
         secondTokenOut = secondTokenReserve - secondTokenAfterSwap; // Calculates the amount of secondToken that will be given to the user after the swap.
 
-        // Safety check to not drain the whole pool
+        // Safety check: Don't drain the entire pool
         if (secondTokenOut == secondTokenReserve) {
             secondTokenOut--; // Subtracts an extra one token if the tokenB is equal to the amount of tokenB in the pool
         }
 
         require(secondTokenOut < secondTokenReserve, "Swap amount too large for pool reserves"); // Requires that the amount of secondToken that will be given to the user is less than the amount of secondToken in the pool.
+        return(secondTokenOut);
 
     }
 
-    function 
+    function swapFirstToken(uint256 _firstTokenAmount) external
+        nonReentrant()
+        returns (uint256 secondTokenOutput)
+    
+    {
+        secondTokenOutput = calculateFirstTokenSwap(_firstTokenAmount); // Calculates the amount of secondToken that will be given to the user after the swap.
+
+        // Transfer tokens from user to pool
+        require(firstToken.transferFrom(msg.sender, address(this), _firstTokenAmount),"Failed to transfer firstToken to the pool"
+        );
+
+        // Update pool reserves
+        firstTokenReserve += _firstTokenAmount;   // Add the amount of firstToken that was swapped to the pool reserve.
+        secondTokenReserve -= secondTokenOutput;  // Subtract the amount of secondToken that was given to the user from the pool reserve.
+        secondToken.transfer(msg.sender, secondTokenOutput); // Transfer the amount of secondToken that was given to the user from the pool to the user.
+
+        // Emit the swap event to log the swap
+        emit Swap(
+            msg.sender,
+            address(firstToken),
+            _firstTokenAmount,
+            address(secondToken),
+            secondTokenOutput,
+            firstTokenReserve,
+            secondTokenReserve,
+            block.timestamp
+        );
+    }
 
 
+    function calculateSecondTokenSwap(uint256 _secondTokenAmount) public view
+        returns (uint256 firstTokenOut)
+    {
+        require(_secondTokenAmount > 0, "Swap amount must be greater than 0"); // Requires that the amount of secondToken that is being swapped is greater than 0.
+        require(firstTokenReserve > 0 && secondTokenReserve > 0, "Invalid pool reserves"); // Requires that the amount of firstToken and secondToken in the pool is greater than 0.
+        require(constantProductK > 0, "Pool not properly initialized");
+        uint256 amountInWithFee = (_secondTokenAmount * 997) / 1000; // 0.3% fee
+        uint256 secondTokenAfterSwap = secondTokenReserve + amountInWithFee; // Calculates the amount of secondToken in the pool after the swap.
         
+        require(secondTokenAfterSwap > 0, "Invalid swap amount: zero reserves after swap");
+        uint256 firstTokenAfterSwap = constantProductK / secondTokenAfterSwap; // Calculates the amount of firstToken in the pool after the swap.
+        firstTokenOut = firstTokenReserve - firstTokenAfterSwap; // Calculates the amount of firstToken that will be given to the user after the swap.
 
+        // Safety check: Don't drain the entire pool
+        if (firstTokenOut == firstTokenReserve) {
+            firstTokenOut--;
+        }
+
+        require(firstTokenOut < firstTokenReserve, "Swap amount too large for pool reserves"); // Requires that the amount of firstToken that will be given to the user is less than the amount of firstToken in the pool.
+        return(firstTokenOut);
+    }
+
+    function swapSecondToken(uint256 _secondTokenAmount) external
+        nonReentrant()
+        returns (uint256 firstTokenOutput)
+    {
+        firstTokenOutput = calculateSecondTokenSwap(_secondTokenAmount); // Calculates the amount of firstToken that will be given to the user after the swap.
+
+        // Transfer tokens from user to pool
+        require(secondToken.transferFrom(msg.sender, address(this), _secondTokenAmount),"Failed to transfer secondToken to the pool"
+        );
+
+        // Update pool reserves
+        secondTokenReserve += _secondTokenAmount;   // Add the amount of secondToken that was swapped to the pool reserve.
+        firstTokenReserve -= firstTokenOutput;  // Subtract the amount of firstToken that was given to the user from the pool reserve.
+        firstToken.transfer(msg.sender, firstTokenOutput); // Transfer the amount of firstToken that was given to the user from the pool to the user.
+
+        // Emit the swap event to log the swap
+        emit Swap(
+            msg.sender,
+            address(secondToken),
+            _secondTokenAmount,
+            address(firstToken),
+            firstTokenOutput,
+            firstTokenReserve,
+            secondTokenReserve,
+            block.timestamp
+        );
+    }
+
+    function removeLiquidity(uint256 _sharesToWithdraw) external
+        nonReentrant()
+        returns(uint256 firstTokenAmount, uint256 secondTokenAmount)
+    {
+        require(_sharesToWithdraw > 0, "Shares to withdraw must be greater than 0");
+        require(_sharesToWithdraw <= userLiquidityShares[msg.sender], "Cannot withdraw more shares than you own");
+        require(totalSharesCirculating > 0, "No liquidity in pool");
+
+        // Calculate the tokens the user is entitled to withdraw
+        firstTokenAmount = (_sharesToWithdraw * firstTokenReserve) / totalSharesCirculating;
+        secondTokenAmount = (_sharesToWithdraw * secondTokenReserve) / totalSharesCirculating;
+
+        // Update state variables
+        totalSharesCirculating -= _sharesToWithdraw;
+        userLiquidityShares[msg.sender] -= _sharesToWithdraw;
+
+        // Update reserves
+        firstTokenReserve -= firstTokenAmount;
+        secondTokenReserve -= secondTokenAmount;
+
+        // Update constant product K
+        constantProductK = firstTokenReserve * secondTokenReserve;
+
+        // Transfer tokens to user
+        require(firstToken.transfer(msg.sender, firstTokenAmount), "Failed to transfer firstToken");
+        require(secondToken.transfer(msg.sender, secondTokenAmount), "Failed to transfer secondToken");
+
+        // Emit RemoveLiquidity event
+        emit RemoveLiquidity(
+            msg.sender,
+            _sharesToWithdraw,
+            firstTokenAmount,
+            secondTokenAmount,
+            block.timestamp
+        );
+
+        return (firstTokenAmount, secondTokenAmount);
+    }
 }
 
 
